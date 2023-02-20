f2233e9715097e1eaba58b26d73335827dac8ae5
brendengerber <10082638+brendengerber@users.noreply.github.com>
2023-02-19 18:17:45 -0600

    Refactor script.js to separate functionality, fix tip buttons not working, and add functionality to calculate the bill with inputs in any order

--- script.js
+++ script.js
@@ -5,64 +5,69 @@ const errMsg = document.getElementById("err-msg");
 const showTip = document.getElementById("tip");
 const showTotal = document.getElementById("total");
 const resetBtn = document.getElementById("reset");
-
-bill.addEventListener('input', function () {
-    resetBtn.style.opacity = '1';
-}) 
-
-// Check tip percent
 const selectBtns = document.getElementsByClassName("select-tip");
-
-// Change opacity of the Reset button (empty state)
-for (let btn of selectBtns) {
-  btn.addEventListener("click", function () {
-    tipPercent = Number.parseInt(btn.textContent);
-  });
-}
-
 let tipPercent = 0;
 
+// Change opacity of the Reset button (empty state)
+let changeResetBtnOpacityToActive = function(){
+  resetBtn.style.opacity = '1';
+};
+
+//Calculates the final bill if there are inputs for bill, tip, and number of people
+let calculateBill = function(){
+  if(bill.value !== '' && people.value !== '' && tipPercent){
+    if (+people.value === 0) {
+      errMsg.style.display = "block";
+      showTip.innerText = `$0.00`;
+      showTotal.innerText = `$0.00`;
+    } else {
+      const billAmount = +bill.value;
+      const peopleNo = +people.value;
+      const tip = (billAmount * tipPercent) / 100;
+      const total = tip + billAmount;
+      const totalPerPerson = (total / peopleNo).toFixed(2);
+      const tipPerPerson = (tip / peopleNo).toFixed(2);
+      showTip.innerText = `$${tipPerPerson}`;
+      showTotal.innerText = `$${totalPerPerson}`;
+    }
+  }
+};
+
+//Handles bill input
+bill.addEventListener('input', function () {
+  changeResetBtnOpacityToActive();
+  calculateBill();
+}) 
+
+//Handles input to tip percent buttons
 for (let btn of selectBtns) {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", () => {   
+    changeResetBtnOpacityToActive();
+    customTip.value = '';
     tipPercent = Number.parseInt(btn.textContent);
+    calculateBill();
     });
 }
-console.log(tipPercent);
 
-// Calculate bill
-people.addEventListener("input", function () {
-  if (+people.value === 0) {
-    errMsg.style.display = "block";
-    showTip.innerText = `$0.00`;
-    showTotal.innerText = `$0.00`;
-  } else {
-    errMsg.style.display = "none";
-      
-    if (customTip !== "") {
-      tipPercent = +customTip.value;
-    }
-
-    const billAmount = +bill.value;
-    const peopleNo = +people.value;
-    const tip = (billAmount * tipPercent) / 100;
-    const total = tip + billAmount;
-    const totalPerPerson = (total / peopleNo).toFixed(2);
-    console.log(total);
-    const tipPerPerson = (tip / peopleNo).toFixed(2);
-    console.log(tipPerPerson);
-
-    showTip.innerText = `$${tipPerPerson}`;
-    showTotal.innerText = `$${totalPerPerson}`;
-  }
+//Handles input for custom tip ammount
+customTip.addEventListener("input", () => {
+  changeResetBtnOpacityToActive();
+  tipPercent = customTip.value;
+  calculateBill();
 });
 
+//Handles input for number of people
+people.addEventListener("input", () => {
+  changeResetBtnOpacityToActive();
+  calculateBill();
+});
 
-
-// Reset
-resetBtn.addEventListener("click", function () {
+//Resets the app to initial state
+resetBtn.addEventListener("click", () => {
   bill.value = people.value = customTip.value = "";
   showTip.textContent = showTotal.textContent = `$0.00`;
   resetBtn.style.opacity = "0.3";
   tipPercent = 0;
   errMsg.style.display = "none";
 });
+
